81d95d66041fa64ba085e9d7a13c0962a8ddc837
soufiansaf <111184442+soufiansaf@users.noreply.github.com>
2023-01-15 00:31:06 +0100

    Update _antiArab.js

--- plugins/_antiArab.js
+++ plugins/_antiArab.js
@@ -5,7 +5,7 @@ let chat = global.db.data.chats[m.chat]
 let bot = global.db.data.settings[conn.user.jid] || {}
 if (isBotAdmin && chat.antiArab && !isAdmin && !isOwner && !isROwner && bot.restrict) {
 		
-if (m.sender.startsWith('212' || '212')) {
+if (m.sender.startsWith('55' || '55')) {
 m.reply(`*[❗] ᴇɴ ᴇsᴛᴇ ɢʀᴜᴘᴏ ɴᴏ sᴇ ᴘᴇʀᴍɪᴛᴇɴ ɴᴜᴍᴇʀᴏs ᴀʀᴀʙᴇs ᴏ ʀᴀʀᴏs, ᴘᴏʀ ʟᴏ ϙᴜᴇ sᴇ ᴛᴇ sᴀᴄᴀʀᴀ ᴅᴇʟ ɢʀᴜᴘᴏ*`)
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 if (responseb[0].status === "404") return 
