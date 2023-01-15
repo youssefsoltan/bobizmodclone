7dedf6566f1a0bdedea4d4dfa39a33987e0146da
soufiansaf <111184442+soufiansaf@users.noreply.github.com>
2023-01-15 11:31:52 +0100

    Update convertidor-tts.js

--- plugins/convertidor-tts.js
+++ plugins/convertidor-tts.js
@@ -1,7 +1,7 @@
 import gtts from 'node-gtts'
 import { readFileSync, unlinkSync } from 'fs'
 import { join } from 'path'
-const defaultLang = 'es'
+const defaultLang = 'ar'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 let lang = args[0]
 let text = args.slice(1).join(' ')
@@ -15,7 +15,7 @@ try { res = await tts(text, lang) }
 catch (e) {
 m.reply(e + '')
 text = args.join(' ')
-if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙰 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝚁 𝙰 𝙽𝙾𝚃𝙰 𝙳𝙴 𝚅𝙾𝚉, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} es Hola Mundo*`
+if (!text) throw `*المرجو كتابة الشيء الذي تود تحويله لأوديو , مثال: ${usedPrefix + command} es مرحبا*`
 res = await tts(text, defaultLang)
 } finally {
 if (res) conn.sendFile(m.chat, res, 'tts.opus', null, m, true)    
@@ -25,7 +25,7 @@ handler.tags = ['tools']
 handler.command = /^g?tts$/i
 export default handler
 
-function tts(text, lang = 'es') {
+function tts(text, lang = 'ar') {
 console.log(lang, text)
 return new Promise((resolve, reject) => {
 try {
