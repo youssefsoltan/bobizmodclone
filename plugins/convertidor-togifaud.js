a3df1b7e40ff4c26ff62be8448295888dc44252f
soufiansaf <111184442+soufiansaf@users.noreply.github.com>
2023-01-19 23:17:13 +0100

    Update convertidor-togifaud.js

--- plugins/convertidor-togifaud.js
+++ plugins/convertidor-togifaud.js
@@ -3,24 +3,14 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
 
 if (!m.quoted) throw `تحويل فيديو الى صورة متحركة GIF 
-
 مثال :  ترسل للبوت الفيديو الذي تريد تحويله لصورة متحركة ثم تشير اليه وتكتب  \n *.togifaud*`
-
 const q = m.quoted || m
-
 let mime = (q.msg || q).mimetype ||''
-
 if (!/(mp4)/.test(mime)) throw `تحويل فيديو الى صورة متحركة GIF 
-
 مثال :  ترسل للبوت الفيديو الذي تريد تحويله لصورة متحركة ثم تشير اليه وتكتب  \n *.togifaud*`
-
 m.reply(global.wait)
-
 let media = await q.download()
-
 conn.sendMessage(m.chat, { video: media, gifPlayback: true, caption:  ʙᴏʙɪᴢ ʙᴏᴛ By Noureddine ouafy*  }, { quoted: m })}
-
 handler.command = ['togifaud']
-
 export default handler
 
