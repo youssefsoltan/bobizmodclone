1b81b00fb2d9ac0f71168a37cad70665adba9067
soufiansaf <111184442+soufiansaf@users.noreply.github.com>
2023-01-20 09:13:34 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,28 +8,28 @@ import axios from 'axios'
 import moment from 'moment-timezone' 
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['573225236629', '💫 Mystic - Collaborator 4 💫', true],
-  ['593959425714', '💫 Mystic - Collaborator 5 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 6 💫', true],  
-  ['5212411719888', '💫 Mystic - Collaborator 7 💫', true],	
-  ['593968585283'],
-  ['5219993404349'],
-  //['593968585383'],	
-  ['5219996125657']
+  ['212605784394', '𝖡𝖮𝖡𝖨𝖹𝖡𝖮𝖳 𝖡𝖸 𝖭𝖮𝖴𝖱𝖤𝖣𝖣𝖨𝖭𝖤 𝖮𝖴𝖠𝖥𝖸', true],
+  ['212605784394', '𝖡𝖮𝖡𝖨𝖹𝖡𝖮𝖳 𝖡𝖸 𝖭𝖮𝖴𝖱𝖤𝖣𝖣𝖨𝖭𝖤 𝖮𝖴𝖠𝖥𝖸', true],
+  ['212605784394', '𝖡𝖮𝖡𝖨𝖹𝖡𝖮𝖳 𝖡𝖸 𝖭𝖮𝖴𝖱𝖤𝖣𝖣𝖨𝖭𝖤 𝖮𝖴𝖠𝖥𝖸', true],
+  ['212605784394', '𝖡𝖮𝖡𝖨𝖹𝖡𝖮𝖳 𝖡𝖸 𝖭𝖮𝖴𝖱𝖤𝖣𝖣𝖨𝖭𝖤 𝖮𝖴𝖠𝖥𝖸', true],
+  ['212605784394', '𝖡𝖮𝖡𝖨𝖹𝖡𝖮𝖳 𝖡𝖸 𝖭𝖮𝖴𝖱𝖤𝖣𝖣𝖨𝖭𝖤 𝖮𝖴𝖠𝖥𝖸', true],
+  ['212605784394', '𝖡𝖮𝖡𝖨𝖹𝖡𝖮𝖳 𝖡𝖸 𝖭𝖮𝖴𝖱𝖤𝖣𝖣𝖨𝖭𝖤 𝖮𝖴𝖠𝖥𝖸', true],
+  ['212605784394', '𝖡𝖮𝖡𝖨𝖹𝖡𝖮𝖳 𝖡𝖸 𝖭𝖮𝖴𝖱𝖤𝖣𝖣𝖨𝖭𝖤 𝖮𝖴𝖠𝖥𝖸', true],  
+  ['212605784394', '𝖡𝖮𝖡𝖨𝖹𝖡𝖮𝖳 𝖡𝖸 𝖭𝖮𝖴𝖱𝖤𝖣𝖣𝖨𝖭𝖤 𝖮𝖴𝖠𝖥𝖸', true],	
+  ['212605784394'],
+  ['212605784394'],
+  //['212605784394'],	
+  ['212605784394']
 ] 
 
-global.suittag = ['5219993404349'] 
-global.prems = ['51995386439'] 
+global.suittag = ['212605784394'] 
+global.prems = ['212605784394'] 
 
-global.packname = '(☞ﾟ∀ﾟ)☞'
-global.author = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
-global.wm = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
-global.igfg = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
-global.wait = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
+global.packname = '𝖡𝖮𝖡𝖨𝖹𝖡𝖮𝖳 𝖡𝖸 𝖭𝖮𝖴𝖱𝖤𝖣𝖣𝖨𝖭𝖤 𝖮𝖴𝖠𝖥𝖸'
+global.author = '𝖡𝖮𝖡𝖨𝖹𝖡𝖮𝖳 𝖡𝖸 𝖭𝖮𝖴𝖱𝖤𝖣𝖣𝖨𝖭𝖤 𝖮𝖴𝖠𝖥𝖸'
+global.wm = '𝖡𝖮𝖡𝖨𝖹𝖡𝖮𝖳 𝖡𝖸 𝖭𝖮𝖴𝖱𝖤𝖣𝖣𝖨𝖭𝖤 𝖮𝖴𝖠𝖥𝖸'
+global.igfg = '𝖡𝖮𝖡𝖨𝖹𝖡𝖮𝖳 𝖡𝖸 𝖭𝖮𝖴𝖱𝖤𝖣𝖣𝖨𝖭𝖤 𝖮𝖴𝖠𝖥𝖸'
+global.wait = '*[❗] سوف يتم تلبية طلبكم بعد قليلا المرجو الانتظار قليلا ...*'
 
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 // 'fiktod' 'BF39D349845E' '675e34de8a' '0b917b905e6f'
@@ -425,20 +425,20 @@ global.flaaa = [
 global.d = new Date(new Date + 3600000)
 global.locale = 'es'
 global.dia = d.toLocaleDateString(locale, { weekday: 'long' })
-global.fecha = d.toLocaleDateString('es', { day: 'numeric', month: 'numeric', year: 'numeric' })
-global.mes = d.toLocaleDateString('es', { month: 'long' })
-global.año = d.toLocaleDateString('es', { year: 'numeric' })
+global.fecha = d.toLocaleDateString('en', { day: 'numeric', month: 'numeric', year: 'numeric' })
+global.mes = d.toLocaleDateString('en', { month: 'long' })
+global.año = d.toLocaleDateString('en', { year: 'numeric' })
 global.tiempo = d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })
 //*****************************
 global.wm2 = `▸ ${dia} ${fecha}\n▸ 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝`
-global.gt = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
-global.mysticbot = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
+global.gt = '𝖡𝖮𝖡𝖨𝖹𝖡𝖮𝖳 𝖡𝖸 𝖭𝖮𝖴𝖱𝖤𝖣𝖣𝖨𝖭𝖤 𝖮𝖴𝖠𝖥𝖸'
+global.mysticbot = '𝖡𝖮𝖡𝖨𝖹𝖡𝖮𝖳 𝖡𝖸 𝖭𝖮𝖴𝖱𝖤𝖣𝖣𝖨𝖭𝖤 𝖮𝖴𝖠𝖥𝖸'
 global.md = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'
 global.mysticbot = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'
-global.waitt = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
-global.waittt = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
-global.waitttt = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
-global.nomorown = '5219993404349'
+global.waitt = '*[❗]  المرجو الانتظار ...*'
+global.waittt = '*[❗] المرجو الانتظار ...*'
+global.waitttt = '*[❗] المرجو الانتظار قليلا...*'
+global.nomorown = '212605784394'
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf']
 global.cmenut = '❖––––––『'
 global.cmenub = '┊✦ '
